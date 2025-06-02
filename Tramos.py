961435491127a152f31b4c2cdf5aa8ab5f427ead
tsirito <tsirito@users.noreply.github.com>
2025-06-02 12:37:12 -0300

    Meti un par de cositas

---
+++ Tramos.py
@@ -0,0 +1,17 @@
+class Tramo():
+    pass
+
+class BsasZarate(Tramo):
+    pass
+class BsasJunin(Tramo):
+    pass
+class BsasAzul(Tramo):
+    pass
+class BsasMdp(Tramo):
+    pass
+class ZarateJunin(Tramo):
+    pass
+class JuninAzul(Tramo):
+    pass
+class AzulMdp(Tramo):
+    pass
--- Vehiculos.py
+++ Vehiculos.py
@@ -1,14 +1,43 @@
 class Vehiculos:
-    pass
+    def __init__(self, velocidad,carga,costoFijo,costoKm,costoKg ):
+        self.velocidad = velocidad
+        self.carga = carga
+        self.costoFijo = costoFijo
+        self.costoKm = costoKm
+        self.costoKg = costoKg
+
     
 class Ferroviario(Vehiculos):
-    pass
+    def __init__(self):
+        Vehiculos.__init__( velocidad = 100,
+        carga = 150000,
+        costoFijo = 100,
+        costoKm = 20,
+        costoKg= 3)
+
     
 class Aereo(Vehiculos):
-    pass
+    def __init__(self):
+        Vehiculos.__init__( velocidad = 600,
+        carga = 5000,
+        costoFijo = 750,
+        costoKm = 40,
+        costoKg= 10)
+
     
 class Automotor(Vehiculos):
-    pass
+    def __init__(self):
+        Vehiculos.__init__( velocidad = 80,
+        carga = 30000,
+        costoFijo = 30,
+        costoKm = 5,
+        costoKg= 1)
+
     
 class Maritimo(Vehiculos):
-    pass
+    def __init__(self):
+        Vehiculos.__init__( velocidad = 40,
+        carga = 100000,
+        costoFijo = 500,
+        costoKm = 15,
+        costoKg= 2)
