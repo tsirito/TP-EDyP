e245949981f0cfb0a786aa0375d5f77f2470634d
Santiago Mainardi <110505198+santiagomainardi@users.noreply.github.com>
2025-06-02 16:26:50 -0300

    Creación de la clase Tramo(), Comentario sobre herencia y creación de Prueba.py para empezar a probar cosas

--- Tramos.py
+++ Tramos.py
@@ -1,17 +1,32 @@
 class Tramo():
+    def __init__(self, origen, destino, tipo, distancia_km):
+        self.origen = origen
+        self.destino = destino
+        self.tipo = tipo
+        self.distancia_km = distancia_km
+        self.restriccion = restriccion
+        self.valor_restriccion = valor_restriccion
     pass
+
+# Acá podemos hacer en vez de cada Tramo una clase, podemos hacer por ejemplo TramoAereo(Tramo):, TramoAutos(Tramo)... y asi... Que todas hereden de la propiedad Tramo, pero cada tipo de Tramo tiene sus propiedades únicas (Reestricción, Valor Reestricción, Cálculo de Costos)
 
 class BsasZarate(Tramo):
     pass
+
 class BsasJunin(Tramo):
     pass
+
 class BsasAzul(Tramo):
     pass
+
 class BsasMdp(Tramo):
     pass
+
 class ZarateJunin(Tramo):
     pass
+
 class JuninAzul(Tramo):
     pass
+
 class AzulMdp(Tramo):
     pass
